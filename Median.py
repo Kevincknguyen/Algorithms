dbe99111ee16d0af38b6d407f1e8e7dc4de99ead
Kevin <kevin.ck.nguyen@gmail.com>
2022-03-22 01:43:55 -0700

    test

---
+++ Median.py
@@ -0,0 +1,23 @@
+
+# 4. Median of Two Sorted Arrays
+
+# Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+# The overall run time complexity should be O(log (m+n)).
+
+
+def findMedianSortedArrays(nums1,nums2):
+    print ("Hello")
+    print (nums1)
+    print(nums2)
+    print (len(nums1)+len(nums2))
+
+    
+
+
+
+
+
+array1=[1,3]
+array2=[2,4]
+findMedianSortedArrays(array1,array2)
