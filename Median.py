0d9428febeaee2ed78b8416eae5f3ee34049b096
Kevin <kevin.ck.nguyen@gmail.com>
2022-03-22 01:59:32 -0700

    median final attempt night of 3/21

--- Median.py
+++ Median.py
@@ -5,19 +5,19 @@
 
 # The overall run time complexity should be O(log (m+n)).
 
+import math
 
 def findMedianSortedArrays(nums1,nums2):
-    print ("Hello")
-    print (nums1)
-    print(nums2)
-    print (len(nums1)+len(nums2))
+    print ("8,posiiton 7")
 
     
+    currentCount=math.floor(len(nums1)/2+len(nums2)/2)
+    print(currentCount)
+    print(math.floor(len(nums1)/2))
+    print(math.floor(len(nums2)/2))
 
 
 
-
-
-array1=[1,3]
-array2=[2,4]
+array1=[1,3,5,7,9]
+array2=[2,4,6,8,10,12,14,15]
 findMedianSortedArrays(array1,array2)
