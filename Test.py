b94c8cbc419544ed2f1254a0b50d43b239fd8580
Kevin <kevin.ck.nguyen@gmail.com>
2022-04-19 22:56:35 -0700

    awd

---
+++ Test.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+def test():
+    print(5)
+    return (0,1)
+
+
+jam=test()
+print(test())
+print(jam)
+
+list=(0,2)
+list= list+test()
+print(list)
+
+return tuples added, with each recursive separated by an array
