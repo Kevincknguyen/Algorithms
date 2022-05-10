3bb0cd377fd9ae3e4b6f5b862573dfa1cfa951c1
Kevin <kevin.ck.nguyen@gmail.com>
2022-05-09 22:55:01 -0700

    Learning a lot about lists (python)

--- Recursive_CombinationSum2.py
+++ Recursive_CombinationSum2.py
@@ -1,18 +1,32 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        print(candidates)
-        print(target)
-        result=[]
-        def recurse(start,current):
-            for position in range(start,len(candidates)):
-                print(position)
-                current.append(candidates[position])
-                if sum(current)==target:
-                    print("VALID SOLUTION PROBABLY APPEND AND ALSO POP THEN REPEAT LOOP")
-                elif sum(current)>target:
-                    print("REMOVE LOGIC MAYBE JUST POP AND REPEAT LOOP")
-                print("THIS IS WHERE YOU DO IT MAYBE LIKE UPDATE START OUTSIDE OF THE FOR LOOP")
-                print("THEN OUTSIDE THIS TOO YOU ADD AN EXTRA RECURSE")
-            print(current)
-        recurse(0,[])
-        return result
+        
+        results=[]
+        def backtrack(uniqueComb,sum,i,counter,results):
+            if sum ==target:
+                results.append(list(uniqueComb))
+                return
+            elif sum>target:
+                return
+    
+            for x in range(i,len(counter)):
+                candidate,freq=counter[x]
+                
+                if freq <=0:
+                    continue
+                    
+                uniqueComb.append(candidate)
+                counter[x]=(candidate,freq-1)
+                
+                backtrack(uniqueComb,sum+candidate,x,counter,results)
+                
+                uniqueComb.pop()
+                counter[x]=(candidate,freq)
+                
+        
+        counter=Counter(candidates)
+        # print(counter)
+        counter = [(c, counter[c]) for c in counter]
+        # print(counter)
+        backtrack(uniqueComb=[],sum=0,i=0,counter=counter,results=results)
+        return results
